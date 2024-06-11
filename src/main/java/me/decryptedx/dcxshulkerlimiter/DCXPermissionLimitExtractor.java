0da261f9a313d72b2fe248de3843d3a4b08d3c26
Excaliburns <tutsandallthat@gmail.com>
2024-06-11 04:46:49 -0400

    And add exception "handling"

--- src/main/java/me/decryptedx/dcxshulkerlimiter/DCXPermissionLimitExtractor.java
+++ src/main/java/me/decryptedx/dcxshulkerlimiter/DCXPermissionLimitExtractor.java
@@ -1,7 +1,10 @@
 package me.decryptedx.dcxshulkerlimiter;
 
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.permissions.PermissionAttachmentInfo;
+
+import java.util.logging.Level;
 
 /**
  * Helper class that encapsulates getting a player's highest effective permission for a permission node
@@ -23,11 +26,18 @@ public class DCXPermissionLimitExtractor {
     public int getLimit(String prefix, int defaultValue) {
         if (player == null) return defaultValue;
 
-        return player.getEffectivePermissions().stream()
-                     .map(PermissionAttachmentInfo::getPermission)
-                     .filter(permission -> permission.startsWith(prefix))
-                     .mapToInt(InventoryListener::integerValueFromPermission)
-                     .max()
-                     .orElse(defaultValue);
+        try {
+            return player.getEffectivePermissions().stream()
+                         .map(PermissionAttachmentInfo::getPermission)
+                         .filter(permission -> permission.startsWith(prefix))
+                         .mapToInt(InventoryListener::integerValueFromPermission)
+                         .max()
+                         .orElse(defaultValue);
+        } catch (NumberFormatException | IndexOutOfBoundsException e) {
+            Bukkit.getLogger().log(Level.SEVERE,
+                    "Permission node " + prefix + " for " + player.getName() +
+                    " has a non-integer value, returning default.");
+            return defaultValue;
+        }
     }
 }
